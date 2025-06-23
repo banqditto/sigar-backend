fc4e5d87501217893eb88a3fa7560781c0f383ea
banqditto <34593552+banqditto@users.noreply.github.com>
2025-06-23 09:26:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('Routing Laravel SIGAR berhasil!'');
+    return view('Routing Laravel SIGAR berhasil!');
 });
 
 Route::get('/login', function () {
